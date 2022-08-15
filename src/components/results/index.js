9befa47ffa2eb452fea37273daf0c8cc9772ee85
Qais Almanasra <qmanasra@gmail.com>
2022-08-15 11:11:02 +0300

    featuered added and problem fixed

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,18 +1,5 @@
 import React from 'react';
 
-// function Results(props)
-// {
-//   let body = JSON.stringify(props.bodyData);
-//     return (
-//         <section data-testid='results'>
-//           <pre>
-//             {props.method === 'GET' ? props.data.name : props.method === 'POST' ? body : props.method === 'PUT' ? body : props.method === 'DELETE' ? 'Deleted' : <div className='loader'></div>}
-//           </pre>
-//         </section>
-//       );   
-// }
-
-// export default Results;
 function Results(props) {
   return (
     <>
